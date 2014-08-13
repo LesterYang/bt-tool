c233bff3177ff5159fcee696c907c6b50a8b2ec5
Lester.Yang <lester.yang@qsitw.com>
2014-08-13 17:01:26 +0800

    modify gitignore

--- iMX5/src/lib/sdp.d
+++
@@ -1,3 +0,0 @@
-src/lib/sdp.d src/lib/sdp.o: ../src/lib/sdp.c ../src/lib/sdp.h
-
-../src/lib/sdp.h:
--- iMX5/src/qsiFunc.d
+++
@@ -1,4 +0,0 @@
-src/qsiFunc.d src/qsiFunc.o: ../src/qsiFunc.c \
- /mnt/hgfs/Win_7/workspace-cpp2/bt-tool/include/qsiFunc.h
-
-/mnt/hgfs/Win_7/workspace-cpp2/bt-tool/include/qsiFunc.h:
